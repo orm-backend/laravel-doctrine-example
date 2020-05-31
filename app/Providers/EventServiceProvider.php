cb51cda56b08a99886812e1804b837baaaa1636a
Vitaliy Kovalenko <vitalykolsky@gmail.com>
2020-05-31 12:37:42 +0300

    Admin menu listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,7 +5,8 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+use ItAces\Web\Events\BeforMenu;
+use ItAces\Admin\AdminMenuListener;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +18,10 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        
+        BeforMenu::class => [
+            AdminMenuListener::class,
         ],
     ];
 
