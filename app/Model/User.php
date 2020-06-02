fa1d49555988a29d9467c069c45ce5ad92c00172
Vitaliy Kovalenko <vitalykolsky@gmail.com>
2020-06-02 10:14:10 +0300

    minor edits

--- app/Model/User.php
+++ app/Model/User.php
@@ -67,4 +67,11 @@ implements Authenticatable, Authorizable, CanResetPassword, MustVerifyEmail, Sof
         });
     }
 
+    /**
+     * 
+     * @return string
+     */
+    public function getName() {
+        return $this->email;
+    }
 }
